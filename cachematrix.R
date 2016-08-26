1f57ced9bf7f6c7fb60d090e772b305d62b37e27
sspitzer1 <sspitzer1@hotmail.com>
2016-08-26 11:00:29 -0400

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions demonstrate lexical scoping
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+
+## Creates a special container matrix and lists 4 functions that
+## set the matrix, get the matrix, solve the matrix (create the inverse)
+## and get the solved matrix
+makeCacheMatrix <- function(x = numeric()) {
+        
+	m <- NULL
+        
+	set <- function(y) {
+                
+		x <<- y
+                
+		m <<- NULL
+        
+		}
+        
+	get <- function() x
+        
+		setsolve <- function(solve) m <<- solve
+        
+		getsolve <- function() m
+        
+		list(set = set, 
+		get = get,
+             
+		setsolve = setsolve,
+             
+		getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## Determine if the matrix has already been solved in cache
+cacheSolve <- function(x,...){
+	m <- x$getsolve()
+	if(!is.null(m)){
+	message("getting cached data")
+	return(m)
+	}
+	data <- x$get()
+	m <- solve(data,...)
+	x$setsolve(m)
+	m
+## Return a matrix that is the inverse of 'x'
 }
